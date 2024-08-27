66be90ff1a52b5e3b8ae9205c99bbcf9c92ab478
Vecta <victoronuchukwu16@gmail.com>
2024-08-27 13:07:08 +0100

    handling user inputs

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ import NewGoal from './components/NewGoal/NewGoal';
 const App = () => {
   const [courseGoals, setCourseGoals] = useState(mockcourseGoals);
   const addNewGoalHandler = (newGoal) => {
-    setCourseGoals(courseGoals.concat(newGoal))
+    // setCourseGoals(courseGoals.concat(newGoal)); //This one used when the new state doesn't depend on the previous
+    setCourseGoals((prevCourseGoals) => { //we only use this when one state is dependent on another.
+      return prevCourseGoals.concat(newGoal);
+    })
     // courseGoals.push(newGoal);
     console.log(courseGoals);
-    
   }
   return (
     <div className='course-goals'>
     <h2>Course Goals</h2>
     <NewGoal  onAddNewGoal={addNewGoalHandler}/> 
-    <GoalList goals={courseGoals}/>   
+    <GoalList goals={courseGoals}/>
     </div>
-  );
+  ); 
 };
 
 export default App;
--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NewGoal.css'
 
 
 const NewGoal = props => {
+  const [enteredText, setEnteredText] = useState('');
 
   const addGoalHandler = event => {
     event.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: 'My new goal'
+      text: enteredText
     }
+
+    setEnteredText(''); 
+    
     props.onAddNewGoal(newGoal);
     // console.log(newGoal);
     }
+
+    const textChangeHandler = event => {
+      setEnteredText(event.target.value);
+    };
+
   return (
     <form className='new-goal' onSubmit={addGoalHandler}>
-        <input type='text'></input>
+        <input type='text' value={enteredText}   onChange={textChangeHandler}></input>
         <button type='submit'>Add A Goal</button>
     </form>
   )
